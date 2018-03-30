a1f0953697b726d1e1fc93900c5916212abea7b7
Gladtone1 <gladstone1@gmail.com>
2018-03-29 17:46:32 -0700

    Fixed compile error for last push

--- src/org/usfirst/frc/team3131/robot/AutonomousLift.java
+++ src/org/usfirst/frc/team3131/robot/AutonomousLift.java
@@ -3,7 +3,9 @@ package org.usfirst.frc.team3131.robot;
 public class AutonomousLift implements AutoCommand {
 
 	public void periodic() {
+		//lift;
 	}
 	public boolean isFinished() {
+		return true;
 	}
 }
